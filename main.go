e0da5c1b04f8015330e983df2c3de7dc799f4ef7
davodhiambo <jesseomolo@yahoo.com>
2024-07-21 18:22:38 +0300

    feat: check and inform the user if they have included not enough or way to many arguments. end program imediately after error message

--- main.go
+++ main.go
@@ -5,9 +5,16 @@ import "os"
 func main() {
 	arg := os.Args[1:]
 
+	// Check if user has included the correct number of arguments (outside program name)
 	if len(arg) != 1 {
+		// Handle instances of no arguments
 		if len(arg) == 0 {
 			PrintLine("Please include the name of a text file as an argument")
+			return
+		} else {
+			// Handle more than one argument
+			PrintLine("You have way too many arguments!\nOnly include one text file as argument")
+			return
 		}
 	}
 }
--- printer.go
+++ printer.go
@@ -4,5 +4,5 @@ import "os"
 
 func PrintLine(str string) {
 	// Write str to terminal
-	os.Stdout.WriteString(str+ "\n")
+	os.Stdout.WriteString(str + "\n")
 }
