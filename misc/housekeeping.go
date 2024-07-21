b8cb59b363e7d846f41e6adfae390959f144fd18
davodhiambo <jesseomolo@yahoo.com>
2024-07-21 19:30:15 +0300

    feat: add ReadFile function to misc package

--- misc/housekeeping.go
+++ misc/housekeeping.go
@@ -1,5 +1,10 @@
 package misc
 
+import (
+	"log"
+	"os"
+)
+
 func CheckExtension(str string) bool {
 	var status bool
 	start := len(str) - 4 // Start index of .txt extension
@@ -9,4 +14,15 @@ func CheckExtension(str string) bool {
 		status = true
 	}
 	return status
+}
+
+func ReadFile(file string) string {
+	// Read file parse
+	content, err := os.ReadFile(file)
+	// Print error & exit program should reading fail
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	return string(content)
 }
