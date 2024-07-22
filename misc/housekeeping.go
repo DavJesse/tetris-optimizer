3e3bc4d5572e409427d5eeb1b34dea21707dca12
davodhiambo <jesseomolo@yahoo.com>
2024-07-22 22:42:29 +0300

    feat: add splitString function to houskeeping.go

--- misc/housekeeping.go
+++ misc/housekeeping.go
@@ -26,3 +26,24 @@ func ReadFile(file string) string {
 
 	return string(content)
 }
+
+func splitString(str, sep string) []string {
+	var result []string
+	var token string
+
+	for i := 0; i < len(str); i++ {
+		// Find instances of separator
+		if i <= len(str)-len(sep) && str[i:i+len(sep)] == sep {
+			result = append(result, token) // Append contents of token to result
+			token = ""                     // Empty token
+			i += len(sep) - 1              // Skip characters of separator
+
+			// Add to token characters that are not part of separator
+		} else {
+			token += string(str[i])
+		}
+	}
+	// Append to result any character that may be in token at the end of loop
+	result = append(result, token)
+	return result
+}
