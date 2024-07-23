a900af7c1ce76bd4898017681b5c9a401438a495
davodhiambo <jesseomolo@yahoo.com>
2024-07-23 15:09:44 +0300

    feat: add TestFourByFour to tetro package files: reinitialze go.mod

--- tetro/validity_help_test.go
+++ tetro/validity_help_test.go
@@ -1,1 +1,18 @@
 package tetro
+
+import "testing"
+
+func TestFourByFour(t *testing.T) {
+	subject := [][]string{
+		{"#...", "#..", "#...", "#..."},
+		{"....", "....", "..##", "..##"},
+	}
+	got := fourByFour(subject)
+	expected := false
+
+	if got != expected {
+		t.Errorf("Got: %t", got)
+		t.Errorf("Expected: %t", expected)
+		t.Errorf("TestFourByFour Failed!")
+	}
+}
