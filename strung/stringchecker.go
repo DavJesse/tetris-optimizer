b051221f3732f775d206170a4cb2c6630929093c
davodhiambo <jesseomolo@yahoo.com>
2024-07-23 23:06:31 +0300

    feat: add Contains to strung stringchecker.go

--- strung/stringchecker.go
+++ strung/stringchecker.go
@@ -1,1 +1,14 @@
 package strung
+
+func Contains(str, subStr string) bool {
+	var status bool
+
+	// Look for instances of subStr
+	for i := 0; i < len(str); i++ {
+		if str[i:i+len(subStr)] == subStr {
+			status = true
+			break // Once found, break for efficiency
+		}
+	}
+	return status
+}
