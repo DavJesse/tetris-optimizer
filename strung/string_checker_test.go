2aaf582374fabd3e44fdf15a474b1fe2fdfe2aff
davodhiambo <jesseomolo@yahoo.com>
2024-07-23 23:27:13 +0300

    feat: add TestContains to strung string_checker_test.go

--- strung/string_checker_test.go
+++ strung/string_checker_test.go
@@ -1,1 +1,15 @@
 package strung
+
+import "testing"
+
+func TestContains(t *testing.T) {
+	subject := "..##"
+	got := Contains(subject, "#")
+	expected := true
+
+	if got != true {
+		t.Errorf("Got: %t", got)
+		t.Errorf("Expected: %t", expected)
+		t.Errorf("TestContains Failed!")
+	}
+}
