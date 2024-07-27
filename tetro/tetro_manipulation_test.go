4b33bf69fd3fb2d438f4009e260e793f17a29de5
davodhiambo <jesseomolo@yahoo.com>
2024-07-27 12:36:40 +0300

    test: add tetro_manipulation_test.go to tetro package

--- tetro/tetro_manipulation_test.go
+++ tetro/tetro_manipulation_test.go
@@ -1,1 +1,40 @@
 package tetro
+
+import "testing"
+
+// func TestTetroTrim(t *testing.T) {
+// 	test := ""
+// 	subject :=
+// 	got :=
+// 	expected :=
+
+// 	if got != expected {
+// 		t.Errorf("Got: %v", got)
+// 		t.Errorf("Expected: %v", expected)
+// 		t.Errorf("%s Failed!", test)
+// 	}
+// }
+
+func TestEmptColumn(t *testing.T) {
+	test := "TestEmptColumn"
+	subject := []string{"#...", "#...", "#...", "#..."}
+	got := occupyCol(subject)
+	expected := []int{0}
+
+	if len(got) != len(expected) {
+		t.Errorf("Got: %v", got)
+		t.Errorf("Expected: %v", expected)
+		t.Errorf("%s Failed!", test)
+		t.Fail()
+	} else {
+		for i := 0; i < len(expected); i++ {
+			if got[i] != expected[i] {
+				t.Errorf("Got: %v", got)
+				t.Errorf("Expected: %v", expected)
+				t.Errorf("%s Failed!", test)
+			}
+		}
+
+	}
+
+}
--- tetro/tetromanipulation.go
+++ tetro/tetromanipulation.go
@@ -1,25 +1,43 @@
 package tetro
 
-func TetroTrim(tet []string) []string {
+import "tetris/strung"
 
-}
+// func TetroTrim(tet []string) []string {
 
-func emptColumn(tet []string) []int {
+// }
+
+func occupyCol(tet []string) []int {
 	var index []int
-	empt := true
+	//empt := false
 	tag := byte('#')
-	row := len(tet)
-	col := len(tet[0])
+	row, col := len(tet), len(tet[0])
 
 	for i := 0; i < col; i++ {
 		for j := 0; j < row; j++ {
 			if tet[j][i] == tag {
-				empt = false
+				//empt = true
+				index = append(index, i)
 				break
-			} else if empt == true && j == row-1 {
-				index = append(index, j)
 			}
 		}
 	}
 	return index
+}
+
+func removeRow(tet []string) []string {
+	for i, row := range tet {
+		if !strung.Contains(row, "#") {
+			if i == 0 {
+				tet = tet[1:]
+				i++
+			} else if i == len(tet)-1 {
+				tet = tet[:i]
+				break
+			} else {
+				tet = append(tet[:i], tet[i+1:]...)
+			}
+		}
+	}
+
+	for i := 0;
 }
