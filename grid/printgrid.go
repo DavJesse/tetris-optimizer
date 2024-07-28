44ddaed378692258f4fcbb0889fa00ca02741058
davodhiambo <jesseomolo@yahoo.com>
2024-07-28 20:04:21 +0300

    feat: add PrintGrid to grid package

--- grid/printgrid.go
+++ grid/printgrid.go
@@ -1,5 +1,11 @@
 package grid
 
+import (
+	"fmt"
+
+	"tetris/misc"
+)
+
 func PrintGrid(size int, tetroSlc [][]string) {
 	grid := make([][]byte, size)
 
@@ -11,7 +17,11 @@ func PrintGrid(size int, tetroSlc [][]string) {
 	for i := range grid {
 		for j := range grid[i] {
 			if grid[i][j] == 0 {
+				misc.Print(".")
+			} else {
+				fmt.Printf("%c", grid[i][j])
 			}
 		}
+		misc.PrintLine("")
 	}
 }
