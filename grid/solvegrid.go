8f6e995b6fcd4bb5e88bae8115fbf7ea0d48cf83
davodhiambo <jesseomolo@yahoo.com>
2024-07-28 17:07:41 +0300

    feat: add canPlace function to solvegrid.go

--- grid/solvegrid.go
+++ grid/solvegrid.go
@@ -1,1 +1,14 @@
 package grid
+
+func canPlace(grid [][]byte, tetromino []string, row, col, size int) bool {
+	for i := range tetromino {
+		for j := range tetromino[i] {
+			if tetromino[i][j] != '.' {
+				if row+i >= size || col+j >= size || grid[row+i][col+j] != 0 {
+					return false
+				}
+			}
+		}
+	}
+	return true
+}
