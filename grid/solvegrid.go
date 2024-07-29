6550ac60c9b765cf9c66aeb3a7d4cc296d41f3c6
davodhiambo <jesseomolo@yahoo.com>
2024-07-29 10:15:54 +0300

    docs: add comments to solvegrid.go.

--- grid/solvegrid.go
+++ grid/solvegrid.go
@@ -1,13 +1,18 @@
 package grid
 
-func Solve(tetrominoes [][]string) int {
+func Solve(tetroSlc [][]string) int {
+
+	// initalize the smallest possible grid,
+	// placing and backtracking tetrominoes,
+	// and incrimentally increasing grid size
 	for size := 2; ; size++ {
-		grid := make([][]byte, size)
+		grid := make([][]byte, size) // Create outer slice
+
 		for i := range grid {
-			grid[i] = make([]byte, size)
+			grid[i] = make([]byte, size) // Adjust size of inner slice
 		}
 
-		if placeTetromino(grid, tetrominoes, 0, size) {
+		if placeTetromino(grid, tetroSlc, 0, size) {
 			return size
 		}
 	}
