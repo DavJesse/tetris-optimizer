2d8ff4d7922575214147feed640af99e526c8082
davodhiambo <jesseomolo@yahoo.com>
2024-08-02 19:20:20 +0300

    testing: add TestPlaceTetromino to tetro_placement_test.go

--- grid/tetro_placement_test.go
+++ grid/tetro_placement_test.go
@@ -3,21 +3,29 @@ package grid
 import "testing"
 
 func TestPlaceTetromino(t *testing.T) {
-		test := "TestPlaceTetromino"
-		grid := make([][]byte, 4)
-		for i := range grid {
-			grid[i] = make([]byte, 4)
-		}
-		subject :=
-		got :=  // Retrieve output for comparison
-		expected :=
-	
-		if got != expected {
-			t.Errorf("Got: %v", got)
-			t.Errorf("Expected: %v", expected)
-			t.Errorf("%s Failed!", test)
-		}
+	test := "TestPlaceTetromino"
+	grid := make([][]byte, 4)
+	for i := range grid {
+		grid[i] = make([]byte, 4)
 	}
+	tetroSlc := [][]string{
+		{
+			"A",
+			"A",
+			"A",
+			"A",
+		},
+	}
+	index, size := 0, 4
+	got := placeTetromino(grid, tetroSlc, index, size) // Retrieve output for comparison
+	expected := true
+
+	if got != expected {
+		t.Errorf("Got: %v", got)
+		t.Errorf("Expected: %v", expected)
+		t.Errorf("%s Failed!", test)
+	}
+}
 
 // func Test(t *testing.T) {
 // 	test := "Test"
