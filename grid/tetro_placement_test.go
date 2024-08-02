8316b86212a7ce29e1b9d897be619cf22f965b2b
davodhiambo <jesseomolo@yahoo.com>
2024-08-02 19:33:13 +0300

    testing: add TestCanPlace to tetro_placement_test.go

--- grid/tetro_placement_test.go
+++ grid/tetro_placement_test.go
@@ -27,6 +27,29 @@ func TestPlaceTetromino(t *testing.T) {
 	}
 }
 
+func TestCanPlace(t *testing.T) {
+	test := "TestCanPlace"
+	grid := make([][]byte, 4)
+	for i := range grid {
+		grid[i] = make([]byte, 4)
+	}
+	tetromino := []string{
+		"A",
+		"A",
+		"A",
+		"A",
+	}
+	row, col, size := 0, 0, 4
+	got := canPlace(grid, tetromino, row, col, size) // Retrieve output for comparison
+	expected := true
+
+	if got != expected {
+		t.Errorf("Got: %v", got)
+		t.Errorf("Expected: %v", expected)
+		t.Errorf("%s Failed!", test)
+	}
+}
+
 // func Test(t *testing.T) {
 // 	test := "Test"
 // 	subject :=
