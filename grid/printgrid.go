044d52127df4c7c707bb5ae67197539b2852dee7
davodhiambo <jesseomolo@yahoo.com>
2024-08-04 17:24:19 +0300

    docs: add comments to printgrid.go

--- grid/printgrid.go
+++ grid/printgrid.go
@@ -4,22 +4,25 @@ import (
 	"tetris/misc"
 )
 
+// Prints solved grid
 func PrintGrid(size int, tetroSlc [][]string) {
+	// Create empty grid of specified size
 	grid := make([][]byte, size)
 
 	for i := range grid {
 		grid[i] = make([]byte, size)
 	}
-	placeTetromino(grid, tetroSlc, 0, size)
+	placeTetromino(grid, tetroSlc, 0, size) // Place tetrominos
 
+	// Print grid
 	for i := range grid {
 		for j := range grid[i] {
 			if grid[i][j] == 0 {
-				misc.Print(".")
+				misc.Print(".") // Replace blanks with '.'s
 			} else {
-				misc.Print(string(grid[i][j]))
+				misc.Print(string(grid[i][j])) // Print alphabetic character
 			}
 		}
-		misc.PrintLine("")
+		misc.PrintLine("") // Print newline to move on to next line
 	}
 }
